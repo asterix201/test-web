e9651e9123e23658e13114607fce0742530700b6
asterix201 <asterix20@yandex.ru>
2019-07-22 22:41:41 +0200

    first commit

---
+++ app/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style>
+	h1 {
+		text-align: center;
+	}
+	img {
+		text-align: center;
+		width:50%;
+		margin: 0 auto;
+	}
+
+	#cont {
+		width: 100%;
+		text-align: center;
+	}
+	</style>
+</head>
+<body>
+	<h1>Тест</h1>
+	<hr>
+	<div id="cont">
+<?php 
+echo '<img src="cat.jpg">';
+?>
+</div>
+</body>
+</html>
+
