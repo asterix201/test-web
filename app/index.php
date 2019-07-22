c325b1b163b96480b962ef280502089e2f7332aa
asterix201 <asterix20@yandex.ru>
2019-07-23 01:24:06 +0300

    Добавил описание задачи, чуть обновил показывание картинок из каталога

--- app/index.php
+++ app/index.php
@@ -22,9 +22,31 @@
 	<h1>Тест</h1>
 	<hr>
 	<div id="cont">
-<?php 
-echo '<img src="cat.jpg">';
+
+<?php
+  $dir = 'images';
+  $imgs_arr = array();
+  // Check if image directory exists
+  if (file_exists($dir) && is_dir($dir) ) {
+    
+      // Get files from the directory
+      $dir_arr = scandir($dir);
+      $arr_files = array_diff($dir_arr, array('.','..') );
+      foreach ($arr_files as $file) {
+        //Get the file path
+        $file_path = $dir."/".$file;
+        // Get extension
+        $ext = pathinfo($file_path, PATHINFO_EXTENSION);
+        if ($ext=="jpg" || $ext=="png" || $ext=="JPG" || $ext=="PNG") {
+          array_push($imgs_arr, $file);
+        }
+        
+      }
+      $count_img_index = count($imgs_arr) - 1;
+      $random_img = $imgs_arr[rand( 0, $count_img_index )];
+  }
 ?>
+<img src="<?php echo $dir."/".$random_img ?>">
 </div>
 </body>
 </html>
